f4893a7760a811d0f661c0566fca7e53c9b56bd2
Camper689 <camper689@gmail.com>
2022-04-30 16:34:53 +0300

    Added spring boot

---
+++ src/main/java/com/cgol/CoolerGameOfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.cgol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolerGameOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolerGameOfLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgol/coolergameoflife/CoolerGameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgol.coolergameoflife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolerGameOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
